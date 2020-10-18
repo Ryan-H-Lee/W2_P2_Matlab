d454c4cb030771beb18dce7657be85efc3a4a17c
Ryan-H-Lee <72281034+Ryan-H-Lee@users.noreply.github.com>
2020-10-18 14:42:32 -0700

    Added Counting Face Value
    Added Code to take rewards and to count up how many faces are in a state

---
+++ getFaceHist.m
@@ -0,0 +1,16 @@
+% state: column vectors of dice where each element is the die value
+% maxFace: how many sides do these die have
+% Hist: column vectors where the first row is the number of die in a state
+%   showing 1, 2nd row is number of 2s etc...
+%this function will take a list of dice and their state and return a column
+
+function hist = getFaceHist(state, maxFace)
+    nStates = size(state,2)
+    %Holds output values
+    hist = zeros(maxFace,nStates);
+    %Check each face value
+    for faceVal = 1:maxFace
+        % how many of faceVal are in each column vector state
+        hist(faceVal,:) = sum(state == faceVal,1)
+    end
+end
